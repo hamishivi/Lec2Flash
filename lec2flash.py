f088dee0241efeb9ffc27857101f88719d16bd2d
Anuj Dhavalikar <chronal@chronal.space>
2019-08-10 17:32:51 +1000

    Adds funcionality to upload file and then print it out

--- lec2flash.py
+++ lec2flash.py
@@ -1,10 +1,15 @@
 from flask import Flask
+
+from flask import render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def index() -> str:
-    return "Welcome to Lec2Flash"
+    return render_template('form.html')
 
-@app.route('/process-file', method=['POST'])
-def process_file():
-    pass
+@app.route('/process-file', methods=['POST'])
+def process_file() -> str:
+    string = request.files['file'].stream.read().decode('utf-8')
+    print(string)
+    return render_template('output.html', string=string)
