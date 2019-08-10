c4d9a0d2b01308996bd3652bff397765a25b9c94
Maddie Wagner <musomaddy@gmail.com>
2019-08-10 22:19:55 +1000

    Modified example questions so that they now work (somewhat) with verbs

--- making_questions.py
+++ making_questions.py
@@ -20,33 +20,35 @@ def get_all_relationship_types(tx):
 
 def make_match(tx):
     # trying some basic conditions for "inside:" what is/are?. What are seems
-    # to work decently (ish) well.
-    ## TODO: maybe flip them??
+    #   to work decently (ish) well.
+    # trying some basic stuff for verbs. Currently is <question> <verb> <what>.
+    #   sometimes where would be better fitted though
+    # TODO: maybe flip them??
     question_gen_dict = {"'definition'":
                          "QUESTION:\n\tWhat is the definition of {}?\nANSWER:"
                          "\n\t{}\n",
                          "'inside'":
-                         "QUESTION:\n\tWhat are {}?\nANSWER:\n\t{}\n"
+                         "QUESTION:\n\tWhat are {}?\nANSWER:\n\t{}\n",
+                         "'VERB: ":
+                         "QUESTION:\n\t{} {} what?\nANSWER:\n\t{}\n"
                          }
-    entities = []
     results = tx.run("MATCH (e1:Entity)-[r:Related]-> (e2:Entity) "
                      "RETURN e1.text as e1, e2.text as e2, r.text as rel")
-    for record in results:
-        entities.append((record["e1"], record["e2"], record["rel"]))
-#     print(entities)
+    rel_n = 2
+    entities = [(record["e1"], record["e2"], record["rel"])
+                for record in results]
 
     for entry in entities:
-        if entry[2] in question_gen_dict:
-            print(question_gen_dict[entry[2]].format(entry[0], entry[1]))
+        if entry[rel_n] in question_gen_dict:
+            print(question_gen_dict[entry[rel_n]].format(entry[0], entry[1]))
+        if entry[rel_n].startswith("'VERB: "):
+            print(question_gen_dict["'VERB: "].format(entry[0],
+                                                      entry[rel_n][
+                                                          len("'VERB: "):],
+                                                      entry[1]))
 
 
 all_types = []
 with driver.session() as session:
     # all_types = session.read_transaction(get_all_relationship_types)
     session.read_transaction(make_match)
-testing_all_types = []
-for t in all_types:
-    if t.startswith("VERB") or t.startswith("PREP"):
-        continue
-    testing_all_types.append(t)
-print(sorted(testing_all_types))
